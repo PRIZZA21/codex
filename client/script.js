e9937761c2728ec46302fcd885c5047892cec964
Priyanshu Jayant <priyanshujayant3121@gmail.com>
2023-01-05 20:08:00 +0530

    upadate the server URL

--- client/script.js
+++ client/script.js
@@ -89,7 +89,7 @@ const handleSubmit = async(e)=>{
   loader(messageDiv); // start of loading
 
   // fetch data from server -> bot's response
-  const response = await fetch('https://codex-6tfi.onrender.com',{
+  const response = await fetch('https://codex-6tfi.onrender.com/',{
     method:'POST',
     headers: {'Content-Type': 'application/json'},
     body: JSON.stringify({
